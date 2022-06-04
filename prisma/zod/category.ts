a4a6d642efe8705a07dfed8bda79cb9962c9cc66
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-06-03 21:44:32 -0300

    Remoção de lib Zod

--- prisma/zod/category.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-
-export const CategoryZodModel = z.object({
-  id: z.string(),
-  name: z.string(),
-  createdAt: z.date().nullish(),
-  updatedAt: z.date().nullish(),
-})
