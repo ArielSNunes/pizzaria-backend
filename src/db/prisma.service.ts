cd9801b231e97c43721412563baafc2e0ce2ef53
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-06-11 12:37:06 -0300

    Ajuste para capturar a variável de ambiente do prisma

--- src/db/prisma.service.ts
+++ src/db/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
@@ -6,6 +7,15 @@ export class PrismaService
 	extends PrismaClient
 	implements OnModuleInit, OnModuleDestroy
 {
+	constructor(private readonly configService: ConfigService) {
+		super({
+			log:
+				configService.get('APP_ENV') === 'DEV'
+					? ['query', 'info', 'warn', 'error']
+					: [],
+		})
+	}
+
 	async onModuleInit() {
 		await this.$connect()
 	}
